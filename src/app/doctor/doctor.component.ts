7c989c9da266994516e7eb193c5603de772780d8
ChandanKM <53951383+ChandanKM@users.noreply.github.com>
2020-02-07 19:00:04 +0530

    07-02-2020 Evening last commit

--- src/app/doctor/doctor.component.ts
+++ src/app/doctor/doctor.component.ts
@@ -5,11 +5,36 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './doctor.component.html',
   styleUrls: ['./doctor.component.scss']
 })
-export class DoctorComponent implements OnInit {
+export class DoctorComponent implements OnInit
+{
+    public addresses: any[] = [{
+    address: '',
+    street: '',
+    city: '',
+    country: ''
+    }];
+
     constructor() { }
 
     ngOnInit() {
 
     }
+
+    addAddress() {
+        this.addresses.push({
+          address: '',
+          street: '',
+          city: '',
+          country: ''
+        });
+      }
+
+      removeAddress(i: number) {
+        this.addresses.splice(i, 1);
+      }
+
+      logValue() {
+        console.log(this.addresses);
+      }
 
 }
